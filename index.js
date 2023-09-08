160d6bea7dcb394b1995a8835644acce54b7f45a
oladejo code02f <oladejofaith866@gmail.com>
2023-09-07 17:46:02 -0700

    added css styles

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello world')
+// create an array of all days of the week
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+// get the value of today's name
+const today = new Date().getDay();
+
+// extract the name from the weekday array
+const currentDayOfTheWeek = weekday[today]
+
+
+const hours = new Date().getHours()
+const minutes = new Date().getMinutes()
+
+const currentTime = `${hours}:${minutes}`
+
+$(document).ready(
+    function () {
+        $("#currentUTCtime").html(currentTime)
+    }
+)
